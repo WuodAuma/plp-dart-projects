71b6e085ad7cebfa32d7076783e6cbba2e25dcf3
WuodAuma <wuodauma2000@gmail.com>
2024-03-31 15:13:53 +0300

    first-dart-datatype-commit

---
+++ datatype.dart
@@ -0,0 +1,9 @@
+int multiplyTwo(int num1, int num2) {
+  return num1 * num2;
+}
+void main() {
+  int a = 5;
+  int b = 7;
+  int product = multiplyTwo(a, b);
+  print("The product of $a and $b is $product");
+}
